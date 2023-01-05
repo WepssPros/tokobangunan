bde5bfbdeaae788f16247ffe8757ca0699641289
WepsPros <reyhandwiambrta@gmail.com>
2023-01-05 19:38:02 +0700

    Reyhan Commit

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -5,20 +5,23 @@ namespace App\Http\Controllers\API;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
-use App\Models\TransactionItem;
+use App\Models\Transactionitem;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Midtrans\Config;
+use Midtrans\Snap;
 
 class TransactionController extends Controller
 {
     public function all(Request $request)
     {
         $id = $request->input('id');
-
+        $limit = $request->input('limit', 6);
         $status = $request->input('status');
 
         if ($id) {
-            $transaction = Transaction::with(['items.product', 'user'])->find($id);
+            $transaction = Transaction::with(['items.product'])->find($id);
 
             if ($transaction)
                 return ResponseFormatter::success(
@@ -33,13 +36,13 @@ class TransactionController extends Controller
                 );
         }
 
-        $transaction = Transaction::with(['items.product', 'user'])->where('users_id', Auth::user()->id)->get();
+        $transaction = Transaction::with(['items.product'])->where('users_id', Auth::user()->id);
 
         if ($status)
             $transaction->where('status', $status);
 
         return ResponseFormatter::success(
-            $transaction,
+            $transaction->paginate($limit),
             'Data list transaksi berhasil diambil'
         );
     }
@@ -50,31 +53,48 @@ class TransactionController extends Controller
      */
     public function checkout(Request $request)
     {
+        $dates = Carbon::now()->format('Y-m-d');
         $request->validate([
             'items' => 'required|array',
-            'items.*.id' => 'exists:products,id',
-            'total_price' => 'required',
-            'shipping_price' => 'required',
-            'status' => 'required|in:PENDING,SUCCESS,CANCELLED,FAILED,SHIPPING,SHIPPED',
+            'items.*.id' => 'exists:food,id',
+            'namapemesan' => 'required',
+            'meja' => 'required',
+            'total' => 'required',
+            'status' => 'required|in:PENDING,ON_DELIVERY,DELIVERED,CANCEL,',
         ]);
 
         $transaction = Transaction::create([
-            'users_id' => Auth::user()->id,
-            'address' => $request->address,
-            'total_price' => $request->total_price,
-            'shipping_price' => $request->shipping_price,
+            'user_id' => Auth::user()->id,
+            'namapemesan' => $request->namapemesan,
+            'tgl' => $dates,
+            'meja' => $request->meja,
+            'total' => $request->total,
             'status' => $request->status
         ]);
 
-        foreach ($request->items as $product) {
+        foreach ($request->items as $food) {
             TransactionItem::create([
-                'users_id' => Auth::user()->id,
-                'products_id' => $product['id'],
-                'transactions_id' => $transaction->id,
-                'quantity' => $product['quantity']
+                'user_id' => Auth::user()->id,
+                'food_id' => $food['id'],
+                'transaction_id' => $transaction->id,
+                'quantity' => $food['quantity'],
             ]);
         }
 
-        return ResponseFormatter::success($transaction->load('items.product'), 'Transaksi berhasil');
+        return ResponseFormatter::success($transaction->load('items.food',), 'Transaksi berhasil',);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        $transaction->update($request->all());
+
+        return ResponseFormatter::success($transaction, 'Transaksi berhasil diperbarui');
     }
 }
